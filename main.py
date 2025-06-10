b3574339532aa3ddc8c03ccb2c30fd193f1d9e65
Psych001 <41246230+Psych001@users.noreply.github.com>
2025-06-11 02:20:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
 import os
+from telegram.ext import ApplicationBuilder, CommandHandler
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update, context):
-    await update.message.reply_text("הבוט פעיל 🎉 שלח לי קישור!")
+    await update.message.reply_text("הבוט חי 🎉")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
-
 app.run_polling()
