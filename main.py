0c18c97e238451c611cad72bb9f4a8126e23855c
Psych001 <41246230+Psych001@users.noreply.github.com>
2025-06-11 02:21:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 async def start(update, context):
     await update.message.reply_text("הבוט חי 🎉")
 
+print("BOT_TOKEN from env:", BOT_TOKEN)
+
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
